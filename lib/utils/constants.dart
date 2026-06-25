10dd00a1e7ff485cfc653abe2a0dcd4e498304d8
DKXAILBY <3047902923@QQ.COM>
2026-06-25 16:27:34 +0800

    bump: v3.15.7 build 121

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,8 +3,8 @@ class AppConstants {
 
   static const String appName = 'LoveGirl';
   static const String baseUrl = 'http://47.121.119.191:3001';
-  static const String versionName = '3.15.6';
-  static const int versionCode = 120;
+  static const String versionName = '3.15.7';
+  static const int versionCode = 121;
 
   // 设计规范
   static const double borderRadius = 14.0;
