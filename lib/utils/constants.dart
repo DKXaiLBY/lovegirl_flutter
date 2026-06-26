d4e4a5848c99491b91e1e2609504c74db3095265
DKXAILBY <3047902923@QQ.COM>
2026-06-26 16:06:20 +0800

    bump: v3.17.1 build 133 — 旅行页面重写

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,8 +3,8 @@ class AppConstants {
 
   static const String appName = 'LoveGirl';
   static const String baseUrl = 'http://47.121.119.191:3001';
-  static const String versionName = '3.17.0';
-  static const int versionCode = 132;
+  static const String versionName = '3.17.1';
+  static const int versionCode = 133;
 
   // 设计规范
   static const double borderRadius = 14.0;
