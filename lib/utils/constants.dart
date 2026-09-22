ba3f95ec49c109a8c508df204d83d7c01a5aa7e6
DKXAILBY <3047902923@QQ.COM>
2026-09-23 01:55:31 +0800

    chore: 版本 bump v3.29.0+157 + CHANGELOG（美食手账/孤儿页复活/TA近况）

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,8 +3,8 @@ class AppConstants {
 
   static const String appName = 'LoveGirl';
   static const String baseUrl = 'http://47.121.119.191:3001';
-  static const String versionName = '3.28.0';
-  static const int versionCode = 156;
+  static const String versionName = '3.29.0';
+  static const int versionCode = 157;
 
   // 设计规范
   static const double borderRadius = 14.0;
