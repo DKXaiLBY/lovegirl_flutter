cbdbea79778b783cfd9292700cd10a6d19ae5d17
DKXAILBY <3047902923@QQ.COM>
2026-09-24 02:45:29 +0800

    chore: 版本 bump v3.31.0+159

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,8 +3,8 @@ class AppConstants {
 
   static const String appName = 'LoveGirl';
   static const String baseUrl = 'http://47.121.119.191:3001';
-  static const String versionName = '3.30.0';
-  static const int versionCode = 158;
+  static const String versionName = '3.31.0';
+  static const int versionCode = 159;
 
   // 设计规范
   static const double borderRadius = 14.0;
