1879c9fc01608c1fc6fd24011a47cbf37207b999
DKXAILBY <3047902923@QQ.COM>
2026-09-26 06:12:57 +0800

    chore(release): v3.33.0+162

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,8 +3,8 @@ class AppConstants {
 
   static const String appName = 'LoveGirl';
   static const String baseUrl = 'http://47.121.119.191:3001';
-  static const String versionName = '3.32.0';
-  static const int versionCode = 161;
+  static const String versionName = '3.33.0';
+  static const int versionCode = 162;
 
   // 设计规范
   static const double borderRadius = 14.0;
